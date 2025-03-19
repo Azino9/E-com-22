3dc42274e3baecd3aa4203e5507e90f62b62758f
MOHIToSAMAL <mohitodomain@gmail.com>
2025-03-19 11:37:33 +0530

    Done the Milestone-22

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const app =require('./app')
+const connectDb=require('./db/db')
+
+process.on("uncaughtException",(err)=>{
+    console.log(`error: ${err.message}`)
+    console.log(`submitting down the server for handling the uncaught exception`)
+
+})
+
+if(process.env.NODE_ENV!=='PRODUCTION'){
+    require('dotenv').config({path:'backend/config/.env'})
+}
+
+connectDb()
+const server =app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+})
+
+//unhandled promise rejection
+process.on('unhandledRejection',(err)=>{
+    console.log(`server is shuttimg down for ${err.message}`)
+
+    server.close(()=>{
+        process.exit(1);
+    })
+})
+
